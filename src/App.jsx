b3e96fe326d069e526480a7dfc81796fa3a1eeac
sakilk130 <sakilk130@gmail.com>
2021-10-15 21:35:51 +0600

    fetch data and style row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
 import './App.css';
+import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './utils/requests';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello from react</h1>
+    <div className="app ">
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
